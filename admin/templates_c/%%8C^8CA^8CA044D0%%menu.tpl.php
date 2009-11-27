38a4df9cf2b95a20887d24545d92533f57692ac5
Vinzenz Hersche <hersche@puzzle.ch>
2009-11-27 13:13:24 +0100

    delete and ignore the compiled template files for the future.. this makes the git-repo more clear.

--- admin/templates_c/%%8C^8CA^8CA044D0%%menu.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2009-11-26 13:49:38
-         compiled from menu.tpl */ ?>
-<div id="menu"><a href="../index.php" >Back to main</a><a href="user.php">Usermanagement</a></div>
--- templates_c/%%8C^8CA^8CA044D0%%menu.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2009-11-27 13:00:02
-         compiled from menu.tpl */ ?>
-<div id="menu"><a href="index.php">Home</a><a href="cards.php">Cards</a><a
-	href="login.php?action=logout">Logout</a><?php if ($this->_tpl_vars['admin']): ?><a
-	href="admin/index.php">Admin</a><?php endif; ?></div>
