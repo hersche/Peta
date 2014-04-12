089cd2fab4d4009851e72fef15aa4c92622ccb7a
skamster <hersche@vmail.me>
2014-04-12 18:33:23 +0200

    getting first outside-plugin-contact :)

--- plugins/examplePluginOutSide.php
+++ plugins/examplePluginOutSide.php
@@ -1,10 +1,23 @@
 <?php
 class examplePluginOutside extends plugin{
+	private $post;
+	private $get;
+	private $currentUser;
+	private $templateObject;
+	private $connection;
+	public function __construct($post, $get, $currentUser, $templateObject, $connection){
+		$this->post = $post;
+	}
+	
 	public function getPluginName(){
-		return "Outside";
+		return "I am outside, but you see me!";
 	}
 	public function getDependensies(){
 		return "blubb";
+	}
+	
+	public function start(){
+		return "Start";
 	}
 }
 
