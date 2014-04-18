21c320730f9a45cbe500e3fa87aaa280f4a160f5
SublimeProphets <christian@thoeni.me>
2014-04-18 12:41:18 +0200

    remove deprecated plugin-stuff

--- index.php
+++ index.php
@@ -4,16 +4,6 @@ if($user->getWelcome()){
 	$messages[] = "Welcome ".$user->getUsername().'. Your last Login was at '.$user->getLastLogin().' from adress '.$user->getLastIp();
 	$user->disableWelcome();
 }
-
-$pluginmanager = new pluginmanager($_GET, $_POST, $user, $connection);
-
-$template->assign("plugins", $pluginmanager->getPlugins());
-if (isset($_GET['plugin'])){
-	$plugin = $pluginmanager->getPluginbyid($_GET['plugin']);
-	$messages[] = $plugin->getPluginName();
-}
-// $var = array("content");
-// $template->_smarty_include("plugins/exampleplugin/templates/content.tpl", $var);
 $template->assign("messages", $messages);
 $template->display('index.tpl');
 ?>
