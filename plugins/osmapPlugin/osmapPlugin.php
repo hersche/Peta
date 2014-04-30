e488f2707d475c34fc8d031f90e98b18408d59fb
Skamster <hersche@vmail.me>
2014-04-30 02:50:48 +0200

    integrade maps/leaflet into a plugin
    support adding, edditing and deleting of pois for now.
    create a poi called start to set start-position.
    LEAFLET ROCKS!!

---
+++ plugins/osmapPlugin/osmapPlugin.php
@@ -0,0 +1,135 @@
+<?php
+/**
+* Ideas for this plugin:
+* make pois shareable
+* name start = startposition
+* May html-content on single points? extended but easy to build in
+* find out how to draw ares. need another model and table then, but would be practical
+*/
+class osmapPluginSkamster extends plugin{
+	
+	private $user;
+	private $template;
+	private $connection;
+	private $id;
+	private $folder;
+	private $dbPrefix;
+	private $poiList;
+    
+	/**
+	 *
+	 * Constructor
+	 * @param array $post all the post-datas..
+	 * @param array $get all the get-datas
+	 * @param unknown_type $currentUser
+	 * @param unknown_type $connection
+	 */
+	public function __construct($id, $currentUser, $templateObject, $folder, $connection) {
+		$this->id = $id;
+		$this -> user = $currentUser;
+		$this -> template = $templateObject;
+		$this -> connection = $connection;
+		$this->folder=$folder;
+		$this->dbPrefix=$this->getDbPrefix();
+		$this->updatePois();
+	}
+	public function deleteInstanceTables(){ 
+		$this -> connection -> exec("DROP TABLE IF EXIST `".$this->getDbPrefix()."pois`");
+	}
+
+	public function getPluginName(){
+		return "osmapPlugin.skamster";
+	}	
+	
+	public function getId(){
+		return $this->id;
+	}
+	/**
+		This method will just be executed on instance plugins.
+	**/
+	public function getPluginDescription() {
+		return "This should work as a usual web/info-site .";
+	}
+
+    
+    public function getHeaderTags(){
+		return array('<script src="http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.js"></script>');
+	}
+    	public function getRequiredCss(){
+		return array("http://cdn.leafletjs.com/leaflet-0.7.2/leaflet.css");
+	}	
+	public function updatePois(){
+		$this->poiList = array();
+        $statement = $this->connection->query("SELECT * FROM `".$this->dbPrefix."pois` WHERE `ownerid`=".$this->user->getId().";");
+        if($statement===False){
+            $statement = array();   
+        }
+		foreach($statement as $row){
+			array_push($this->poiList,new poi($row['id'],$row['name'],$row['position'],$row['ownerid']));
+		}
+	}
+    
+    	public function insertPoi($name, $position){
+		$this->connection->exec("INSERT INTO `".$this->dbPrefix."pois` (`name`, `position`, `ownerid`) VALUES ('" . $name . "', '" .$position."', '" .$this->user->getId()."');");
+		$this->updatePois();
+	}
+	
+	public function deletePoi($id){
+		$this->connection->exec("DELETE FROM `".$this->dbPrefix."pois` WHERE `id` = " . $id . "; ");
+		$this->updatePois();
+	}
+	
+	public function editPoi($id, $name, $position){
+		$this->connection->exec("UPDATE `".$this->dbPrefix."pois` SET `name` =  '" . $name . "',`position`='" .$position ."',`ownerid`='" .$this->user->getId(). "' WHERE `id`=".$id . " LIMIT 1 ;");
+		$this->updatePois();
+	}
+    
+    public function getStartPoi(){
+        foreach($this->poiList as $poi){
+            if($poi->name=="start"){
+                return $poi;
+            }
+        }
+    }
+	public function start(){
+        $this -> connection -> exec("CREATE TABLE IF NOT EXISTS `".$this->getDbPrefix()."pois` (
+			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`name` text NOT NULL,
+			`position` text NOT NULL,
+			`zoom` int(3) NOT NULL,
+			`ownerid` int(11) NOT NULL,
+			PRIMARY KEY (`id`)
+		)");
+        if((isset($_POST['createPoiName']))&&($_POST['createPoiPosition'])){
+            $this->insertPoi($_POST['createPoiName'], $_POST['createPoiPosition']);
+        }
+        elseif(isset($_GET['delPoi'])){
+            // TODO such stuff is unprotected!
+            $this->deletePoi($_GET['delPoi']);  
+        }
+        elseif((isset($_POST['editPoiName']))&&(isset($_POST['editPoiPosition']))&&(isset($_POST['editPoiId']))){
+            $this->editPoi($_POST['editPoiId'], $_POST['editPoiName'], $_POST['editPoiPosition']);
+        }
+        $this->template->assign("startPoi", $this->getStartPoi());
+        $this->template->assign("poiList", $this->poiList);
+        $this->template->assign("pluginId", $this->getId());
+        $this->template->display($this->folder.'osmapPlugin.tpl');
+	}
+}
+/**
+* Dataobject represent DB
+* Better do not use public variables like this, do getter and setter. This is just faster.
+**/
+class poi{
+	public $id;
+	public $name;
+    public $position;
+    public $zoom;
+    public $ownerId;
+	public function __construct($id,$name,$position,$ownerId){
+		$this->id = $id;
+		$this->name=$name;
+		$this->position=$position; 
+        $this->ownerId = $ownerId; }
+}
+?>
---
+++ plugins/osmapPlugin/osmapPlugin.tpl
@@ -0,0 +1,70 @@
+<script type="text/javascript">
+    
+    dojo.addOnLoad(function(){
+    // create a map in the "map" div, set the view to a given place and zoom
+        {if $startPoi}
+        var map = L.map('map').setView({$startPoi->position}, 6);
+        {else}
+		var map = L.map('map').setView([51.505, -0.09], 4);
+         {/if}
+{literal}
+		L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+			id: 'examples.map-9ijuk24y'
+		}).addTo(map);
+
+		L.circle([51.508, -0.11], 500, {
+			color: 'red',
+			fillColor: '#f03',
+			fillOpacity: 0.5
+		}).addTo(map).bindPopup("I am a circle.");
+
+		L.polygon([
+			[51.509, -0.08],
+			[51.503, -0.06],
+			[51.51, -0.047]
+		]).addTo(map).bindPopup("I am a polygon.");
+{/literal}
+{if $poiList}
+{foreach from=$poiList item=poi}
+ 		L.marker({$poi->position}).addTo(map)
+			.bindPopup("{$poi->name} <br /><a href='plugin.php?plugin={$pluginId}&amp;delPoi={$poi->id}'>Delete</a>");
+ 
+ {/foreach}
+
+{/if}
+
+		var popup = L.popup();
+
+		function onMapClick(e) {
+            var pos = L.latLng(e.latlng);
+			popup
+				.setLatLng(e.latlng)
+				.setContent('<form action="plugin.php?plugin={$pluginId}" method="post"><input type="text" name="createPoiName" /><input type="hidden" name="createPoiPosition" value="['+pos.lat+','+pos.lng+']" /><input type="submit" value="Create Poi" /></form>' + '['+pos.lat+','+pos.lng+']') {literal}
+				.openOn(map);
+		}
+
+		map.on('click', onMapClick);
+    });
+    {/literal}
+</script>
+
+<div id="map" style="width: 600px; height: 400px"></div>
+
+<h2>Your Pois</h2>
+<p>Just click into the map to create a POI. Call one "start" to set your start position.</p>
+{if $poiList}
+{foreach from=$poiList item=poi}
+<form action="plugin.php?plugin={$pluginId}" method="post">
+    <input type="text" name="editPoiName" value="{$poi->name}" />
+    <input type="hidden" name="editPoiId" value="{$poi->id}" />
+    <input type="text" readonly name="editPoiPosition" value="{$poi->position}" />
+    <input type="submit" value="Edit Poi" /></form>
+ 		<a href='plugin.php?plugin={$pluginId}&amp;delPoi={$poi->id}' class="button delete">Delete</a>
+ 
+ {/foreach}
+
+{/if}
